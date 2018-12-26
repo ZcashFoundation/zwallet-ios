ad6202504c033065bc8eeb3b98c6a018db49a0cd
loj <j.lotzmann@gmx.ch>
2018-12-26 21:39:00 +0100

    added PIN view

---
+++ ZWallet/UIElements/ZWPinDotView.swift
@@ -0,0 +1,178 @@
+//
+//  ZWPinDotView.swift
+//  ZWallet
+//
+//  Created by loj on 26.12.18.
+//  Copyright © 2018 XMR Systems LLC. All rights reserved.
+//
+
+import UIKit
+
+
+@IBDesignable
+public class ZWPinDotView: UIView {
+
+    @IBInspectable
+    public var inputDotCount = 0 {
+        didSet {
+            setNeedsDisplay()
+        }
+    }
+
+    @IBInspectable
+    public var totalDotCount = 6 {
+        didSet {
+            setNeedsDisplay()
+        }
+    }
+
+    @IBInspectable
+    public var strokeColor = UIColor.darkGray {
+        didSet {
+            setNeedsDisplay()
+        }
+    }
+
+    @IBInspectable
+    public var fillColor = UIColor.darkGray {
+        didSet {
+            setNeedsDisplay()
+        }
+    }
+
+    fileprivate var radius: CGFloat = 6
+    fileprivate let spacingRatio: CGFloat = 2
+    fileprivate let borderWidthRatio: CGFloat = 1 / 5
+
+    public override func draw(_ rect: CGRect) {
+        super.draw(rect)
+
+        strokeColor.setStroke()
+        fillColor.setFill()
+
+        let borderWidth = radius * borderWidthRatio
+        let isOdd = (totalDotCount % 2) != 0
+        let positions = getDotPositions(isOdd)
+
+        for (index, position) in positions.enumerated() {
+            if index < inputDotCount {
+                let pathToFill = UIBezierPath(circleWithCenter: position, radius: (radius + borderWidth / 2), lineWidth: borderWidth)
+                pathToFill.fill()
+            } else {
+                let pathToStroke = UIBezierPath(circleWithCenter: position, radius: radius, lineWidth: borderWidth)
+                pathToStroke.stroke()
+            }
+        }
+    }
+
+    private var shakeCount = 0
+    private var shakeDirectionLeft = false
+
+    public func shakeAnimationWithCompletion(_ completion: @escaping () -> ()) {
+        let maxShakeCount = 5
+        var duration = 0.10
+
+        let centerX = bounds.midX
+        let centerY = bounds.midY
+        var moveX: CGFloat = 5
+
+        if shakeCount == 0 || shakeCount == maxShakeCount {
+            duration *= 0.5
+        } else {
+            moveX *= 2
+        }
+        shakeAnimation(withDuration: duration, animations: {
+            if !self.shakeDirectionLeft {
+                self.center = CGPoint(x: centerX + moveX, y: centerY)
+            } else {
+                self.center = CGPoint(x: centerX - moveX, y: centerY)
+            }
+        }) {
+            if self.shakeCount >= maxShakeCount {
+                self.shakeAnimation(withDuration: duration, animations: {
+                    let realCenterX = self.superview!.bounds.midX
+                    self.center = CGPoint(x: realCenterX, y: centerY)
+                }) {
+                    self.shakeDirectionLeft = false
+                    self.shakeCount = 0
+                    completion()
+                }
+            } else {
+                self.shakeCount += 1
+                self.shakeDirectionLeft = !self.shakeDirectionLeft
+                self.shakeAnimationWithCompletion(completion)
+            }
+        }
+    }
+
+    public override func awakeFromNib() {
+        super.awakeFromNib()
+        backgroundColor = UIColor.clear
+    }
+
+    public override func prepareForInterfaceBuilder() {
+        super.prepareForInterfaceBuilder()
+    }
+
+    public override func layoutSubviews() {
+        super.layoutSubviews()
+        updateRadius()
+        setNeedsDisplay()
+    }
+}
+
+
+private extension ZWPinDotView {
+
+    func shakeAnimation(withDuration duration: TimeInterval, animations: @escaping () -> (), completion: @escaping () -> ()) {
+        UIView.animate(withDuration: duration, delay: 0, usingSpringWithDamping: 0.01, initialSpringVelocity: 0.35, options: UIView.AnimationOptions(), animations: {
+            animations()
+        }) { _ in
+            completion()
+        }
+    }
+
+    func updateRadius() {
+        let width = bounds.width
+        let height = bounds.height
+        radius = height / 2 - height / 2 * borderWidthRatio
+        let spacing = radius * spacingRatio
+        let count = CGFloat(totalDotCount)
+        let spaceCount = count - 1
+        if (count * radius * 2 + spaceCount * spacing > width) {
+            radius = floor((width / (count + spaceCount)) / 2)
+        } else {
+            radius = floor(height / 2);
+        }
+        radius = radius - radius * borderWidthRatio
+    }
+
+    func getDotPositions(_ isOdd: Bool) -> [CGPoint] {
+        let centerX = bounds.midX
+        let centerY = bounds.midY
+        let spacing = radius * spacingRatio
+        let middleIndex = isOdd ? (totalDotCount + 1) / 2 : (totalDotCount) / 2
+        let offSet = isOdd ? 0 : -(radius + spacing / 2)
+        let positions: [CGPoint] = (1...totalDotCount).map { index in
+            let i = CGFloat(middleIndex - index)
+            let positionX = centerX - (radius * 2 + spacing) * i + offSet
+            return CGPoint(x: positionX, y: centerY)
+        }
+        return positions
+    }
+}
+
+
+internal extension UIBezierPath {
+    convenience init(circleWithCenter center: CGPoint,
+                     radius: CGFloat,
+                     lineWidth: CGFloat)
+    {
+        self.init(arcCenter: center,
+                  radius: radius,
+                  startAngle: 0,
+                  endAngle: 2.0 * CGFloat(Double.pi),
+                  clockwise: false)
+        self.lineWidth = lineWidth
+    }
+}
---
+++ ZWallet/UIElements/ZWProgressBar.swift
@@ -0,0 +1,182 @@
+//
+//  ZWProgressBar.swift
+//  ZWallet
+//
+//  Created by loj on 23.12.18.
+//  Copyright © 2018 XMR Systems LLC. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+
+@IBDesignable
+public class ZWProgressBar: UIView {
+
+    @IBInspectable
+    public var totalSteps = 4 {
+        didSet {
+            self.setup()
+            setNeedsDisplay()
+        }
+    }
+
+    /// Current step being in progress.
+    /// Must be in range 0...totalSteps
+    @IBInspectable
+    public var currentStep = 1 {
+        didSet {
+            setNeedsDisplay()
+        }
+    }
+
+    @IBInspectable
+    public var borderWidth: CGFloat = 2.0 {
+        didSet {
+            setNeedsDisplay()
+        }
+    }
+
+    @IBInspectable
+    public var borderColor: UIColor = .lightGray {
+        didSet {
+            setNeedsDisplay()
+        }
+    }
+
+    @IBInspectable
+    public var fillColor: UIColor = Colors.zwalletColor {
+        didSet {
+            setNeedsDisplay()
+        }
+    }
+
+    fileprivate let radius: CGFloat = 8
+    fileprivate let dividerLength: CGFloat = 16
+    fileprivate let space: CGFloat = 3
+
+    public override func draw(_ rect: CGRect) {
+        super.draw(rect)
+
+        self.drawDividers()
+        self.drawIndicators()
+    }
+
+    public override func awakeFromNib() {
+        super.awakeFromNib()
+        self.setVisualDefaults()
+    }
+
+    public override func prepareForInterfaceBuilder() {
+        self.setVisualDefaults()
+    }
+
+    public override func layoutSubviews() {
+        super.layoutSubviews()
+        self.setup()
+        setNeedsDisplay()
+    }
+
+    private func setVisualDefaults() {
+
+    }
+
+    private var dividers = [UIView]()
+    private var indicators = [UIView]()
+
+    private func setup() {
+        self.subviews.forEach { $0.removeFromSuperview() }
+        self.setupDividers()
+        self.setupIndicators()
+    }
+
+    private func setupDividers() {
+        let xOffset = self.contentXOffset() + 2 * self.radius + space
+        let xDelta = 2 * self.radius + space + self.dividerLength + space
+        let yOffset = self.radius - (self.borderWidth / 2)
+        self.dividers = [UIView]()
+        for i in 0 ..< self.totalSteps - 1 {
+            let divider = UIView(frame: CGRect(x: xOffset + xDelta * CGFloat(i),
+                                               y: yOffset,
+                                               width: self.dividerLength,
+                                               height: self.borderWidth))
+            divider.backgroundColor = self.borderColor
+            self.dividers.append(divider)
+            self.addSubview(divider)
+        }
+
+        self.indicators = (0 ..< self.totalSteps).map { _ in UIView() }
+    }
+
+    private func setupIndicators() {
+        let xOffset = self.contentXOffset()
+        let xDelta = 2 * self.radius + space + self.dividerLength + space
+        let yOffset = CGFloat(0)
+
+        self.indicators = [UIView]()
+
+        for i in 0 ..< self.totalSteps {
+            let indicator = UIView(frame: CGRect(x: xOffset + xDelta * CGFloat(i),
+                                                 y: yOffset,
+                                                 width: 2 * self.radius,
+                                                 height: 2 * self.radius))
+            indicator.backgroundColor = .clear
+            indicator.layer.cornerRadius = self.radius
+            indicator.layer.borderWidth = self.borderWidth
+            indicator.layer.borderColor = self.borderColor.cgColor
+
+            self.indicators.append(indicator)
+            self.addSubview(indicator)
+        }
+    }
+
+    private func contentWidth() -> CGFloat {
+        return (2 * self.radius) * CGFloat(self.totalSteps)
+            + (self.space + self.dividerLength + self.space) * CGFloat(self.totalSteps - 1)
+    }
+
+    private func contentXOffset() -> CGFloat {
+        return (self.frame.width - self.contentWidth()) / 2
+    }
+}
+
+
+private extension ZWProgressBar {
+
+    private func drawDividers() {
+        self.drawProcessedDividers()
+        self.drawUnprocessedDividers()
+    }
+
+    private func drawProcessedDividers() {
+        if self.currentStep < 2 {
+            return
+        }
+        let max = min(self.dividers.count, self.currentStep - 1)
+        for i in 0 ..< max {
+            let divider = self.dividers[i]
+            divider.layer.borderColor = self.fillColor.cgColor
+            divider.layer.backgroundColor = self.fillColor.cgColor
+        }
+    }
+
+    private func drawUnprocessedDividers() {
+    }
+
+    private func drawIndicators() {
+        self.drawProcessedIndicators()
+        self.drawUnprocessedIndicators()
+    }
+
+    private func drawProcessedIndicators() {
+        let max = min(self.indicators.count, self.currentStep)
+        for i in 0 ..< max {
+            let indicator = self.indicators[i]
+            indicator.layer.borderColor = self.fillColor.cgColor
+            indicator.layer.backgroundColor = self.fillColor.cgColor
+        }
+    }
+
+    private func drawUnprocessedIndicators() {
+    }
+}
